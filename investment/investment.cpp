753dd6ac62f1d0cb97c250d0d246d2573188660a
Mariano Bessone <marjobe@gmail.com>
2014-09-05 17:58:22 -0300

    Good improvement in investment solution.

--- investment/investment.cpp
+++ investment/investment.cpp
@@ -1,103 +1,141 @@
 #include <iostream>
-#include <cassert>
 #include <array>
 #include <list>
+#include <algorithm>
 
-const unsigned int MONTHS_NUMBER = 12u;
+typedef float Money;
+
+enum MonthsInvestment
+{
+    ONE = 1,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGTH,
+    NINE,
+    TEN,
+    ELEVEN,
+    TWELVE
+};
 
 typedef float Rate;
-typedef float Money;
-typedef std::array<Rate, MONTHS_NUMBER> RatesTable;
+typedef std::array<Rate, TWELVE> RatesTable;
 
-/*
-Rate getRate(const RatesTable& table, unsigned int monthsInvestment)
-{
-    assert(monthsInvestment > 0u && monthsInvestment <= MONTHS_NUMBER);
-    return table[monthsInvestment - 1u];
-}
-*/
+typedef MonthsInvestment Action;
+typedef std::list<Action> Actions;
 
-/**
- * @brief Investment class.
- */
-class Investment
+class InvestmentFunction
 {
 public:
 
-    /**
-     * @brief Constructor.
-     * @param[in] ratesTable The rates table to calculate the investment.
-     * @param[in] moneyPerMonth The money per month destinates to do an investment.
-     */
-    Investment(const RatesTable& ratesTable, Money moneyPerMonth) :
-        _ratesTable(ratesTable),
-        _moneyPerMonth(moneyPerMonth)
+    InvestmentFunction(const RatesTable& ratesTable) :
+        _ratesTable(ratesTable)
     {}
 
-    typedef unsigned int Action;
-    typedef std::list<Action> Actions;
-
-    void getBestInvestment(Actions& actions, Money& totalMoney)
+    Money execute(Money initialAmount, Money investmentPerMonth, MonthsInvestment months) const
     {
-        MoneyMonth moneyMonth = {_moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth, _moneyPerMonth};
-        getBestBacktracking(0u, moneyMonth, actions);
-        totalMoney = moneyMonth[MONTHS_NUMBER - 1u];
+        Money monthsSum(0.f);
+        for (MonthsInvestment i = months; i > 0;)
+        {
+            monthsSum += _getRate(i);
+            i = MonthsInvestment(i - 1);
+        }
+
+        return initialAmount * _getRate(months) + investmentPerMonth * monthsSum;
+    }
+
+    Money execute(Money initialAmount, Money investmentPerMonth, MonthsInvestment months, unsigned int times) const
+    {
+        Money total(initialAmount);
+        for (unsigned int i = 0u; i < times; ++i)
+        {
+            total = execute(total, investmentPerMonth, months);
+        }
+
+        return total;
+    }
+
+    Money getBest(Money initialAmount, Money investmentPerMonth, unsigned int totalMonthsOfInvestment,
+                  Actions& actions) const
+    {
+        Money maxBenefits(initialAmount);
+        if(totalMonthsOfInvestment > 0)
+        {
+            Actions bestActions;
+            for (MonthsInvestment m = ONE; m <= std::min(totalMonthsOfInvestment, unsigned(TWELVE));)
+            {
+                Money currentBenefits = execute(initialAmount, investmentPerMonth, m);
+                Actions currentActions = actions;
+                currentActions.emplace_back(m);
+                currentBenefits = getBest(currentBenefits, investmentPerMonth, totalMonthsOfInvestment - m,
+                                           currentActions);
+
+                if (currentBenefits > maxBenefits)
+                {
+                    maxBenefits = currentBenefits;
+                    bestActions = currentActions;
+                }
+
+                // Advance to the next investment month.
+                m = MonthsInvestment(m + 1);
+            }
+            actions = bestActions;
+        }
+
+        return maxBenefits;
     }
 
 private:
 
-    /** Table that has the money per month. */
-    typedef std::array<Money, MONTHS_NUMBER> MoneyMonth;
-
-    /***/
-    void getBestBacktracking(unsigned int month, MoneyMonth& moneyMonth, Actions& actions)
+    inline Rate _getRate(MonthsInvestment months) const
     {
-        if(month < MONTHS_NUMBER - 1u)
-        {
-            MoneyMonth theBestMoneyMonth;
-            Actions theBestActions;
-            for (unsigned int i = 0u; i < MONTHS_NUMBER - 1u - month; ++i)
-            {
-                MoneyMonth currentMoneyMonth = moneyMonth;
-                Actions currentActions = actions;
-                const unsigned int monthsInvestment(i + 1u);
-
-                currentMoneyMonth[month + monthsInvestment] += currentMoneyMonth[month] * (1.f + _ratesTable[i] / 12.f / 100.f * monthsInvestment);
-                currentActions.emplace_back(monthsInvestment);
-                getBestBacktracking(month + 1u, currentMoneyMonth, currentActions);
-                if (currentMoneyMonth[MONTHS_NUMBER - 1u] > theBestMoneyMonth[MONTHS_NUMBER - 1u])
-                {
-                    theBestMoneyMonth = currentMoneyMonth;
-                    theBestActions = currentActions;
-                }
-            }
-            moneyMonth = theBestMoneyMonth;
-            actions = theBestActions;
-        }
+        return _ratesTable[months-1];
     }
 
-    /** Data members. */
-    RatesTable      _ratesTable;
-    Money           _moneyPerMonth;
-    
+    const RatesTable _ratesTable;
 };
 
 
-
-int main(int argc, char const *argv[])
+int main()
 {
-    RatesTable hsbcTable = {20.f, 20.75f, 21.5f, 22.25f, 23.f, 23.75f, 23.75f, 23.75f, 23.75f, 23.75f, 23.75f, 24.5f};
-    Investment investment(hsbcTable, 2000.f);
-    Investment::Actions actions;
-    Money earnMoney;
-    investment.getBestInvestment(actions, earnMoney);
-
-    std::cout << "You earn $ " << earnMoney << std::endl;
-
-    unsigned int i(0u);
-    for (Investment::Action action : actions)
+    const RatesTable hsbcTable =
     {
-        std::cout << "Month " << i++ << ": deposit to " << action << " months." << std::endl;
+        1.f + 20.f   / 12.f / 100.f,
+        1.f + 20.75f / 12.f / 100.f * 2,
+        1.f + 21.5f  / 12.f / 100.f * 3,
+        1.f + 22.25f / 12.f / 100.f * 4,
+        1.f + 23.f   / 12.f / 100.f * 5,
+        1.f + 23.75f / 12.f / 100.f * 6,
+        1.f + 23.75f / 12.f / 100.f * 7,
+        1.f + 23.75f / 12.f / 100.f * 8,
+        1.f + 23.75f / 12.f / 100.f * 9,
+        1.f + 23.75f / 12.f / 100.f * 10,
+        1.f + 23.75f / 12.f / 100.f * 11,
+        1.f + 24.5f  / 12.f / 100.f * 12
+    };
+
+    InvestmentFunction hsbcInvFunction(hsbcTable);
+    const Money initialValue = 27093.5f;
+    const Money investPM = 2000.f;
+
+    std::cout << hsbcInvFunction.execute(0, investPM, TWELVE, 1u) << std::endl;
+    std::cout << hsbcInvFunction.execute(0, investPM, SIX, 2u) << std::endl;
+    std::cout << hsbcInvFunction.execute(0, investPM, FOUR, 3u) << std::endl;
+    std::cout << hsbcInvFunction.execute(0, investPM, THREE, 4u) << std::endl;
+    std::cout << hsbcInvFunction.execute(0, investPM, TWO, 6u) << std::endl;
+    std::cout << hsbcInvFunction.execute(0, investPM, ONE, 12u) << std::endl;
+
+    Actions actions;
+    const Money bestProfit = hsbcInvFunction.getBest(0, investPM, 12u, actions);
+    std::cout << "Best profit: " << bestProfit << std::endl;
+
+    unsigned int op(0u);
+    for(Action action : actions)
+    {
+        std::cout << "Action " << ++op << " : do the inversion to " << action << " months." << std::endl;
     }
 
     return 0;
